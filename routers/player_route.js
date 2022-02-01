79eede8e216ec430f5c77abba00101182942bea2
Raja Gond <rajagond@cse.iitb.ac.in>
2022-02-01 23:43:28 +0530

    part C maybe

--- routers/player_route.js
+++ routers/player_route.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const ctrVenue = require('../controllers/player.controllers');
+const ctrPlayer = require('../controllers/player.controllers');
 
-router.get('/:player_id', ctrVenue.getPlayer);
+router.get('/:player_id', ctrPlayer.getPlayer);
+router.get('/:player_id/batdetails', ctrPlayer.getPlayerDetails);
+router.get('/:player_id/batgraphinfo', ctrPlayer.getPlayerInformationInGraph);
+router.get('/:player_id/bowldetails', ctrPlayer.getBowlingDetails);
+router.get('/:player_id/bowlgraphinfo', ctrPlayer.getBowlingInformationInGraph);
 module.exports = router;
