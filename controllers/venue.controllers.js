0b6c44100077bfa45355b3db9e6ff012097bb4ee
Raja Gond <rajagond@cse.iitb.ac.in>
2022-02-03 07:51:47 +0530

    except B3, B4, C1, C2 , E2.b, E2.c, F all done maybe

--- controllers/venue.controllers.js
+++ controllers/venue.controllers.js
@@ -9,6 +9,7 @@ const getVenue = function (request, response) {
         else response.status(200).json(results.rows)
     })
 }
+/*
 //venues/:venue_id/basic
 const getVenueInformation = function (request, response) {
     const venue_id = parseInt(request.params.venue_id)
@@ -58,16 +59,55 @@ const getVenueInformation = function (request, response) {
       })
       })
 }
+*/
+//venues/:venue_id/basic
+const getVenueInformation = function (request, response) {
+    const venue_id = parseInt(request.params.venue_id)
+    const query = {
+        text: `SELECT venue_name, city_name, country_name, capacity, num_matches, high, low, runs_chased
+		FROM (SELECT venue_name, city_name, country_name, capacity, venue_id FROM venue WHERE venue_id = $1) AS a
+		INNER JOIN
+		(SELECT COUNT(DISTINCT venue_match.match_id) AS num_matches, MAX(score.total_scored) AS high, MIN(score.total_scored) AS low, venue_id
+		FROM (SELECT * FROM match WHERE venue_id = $1) AS venue_match
+		LEFT JOIN
+		(SELECT match_id, SUM(runs_scored + extra_runs) AS total_scored
+		FROM ball_by_ball
+		GROUP BY match_id, innings_no) AS score
+		ON venue_match.match_id = score.match_id
+		GROUP By venue_id) AS b
+		ON a.venue_id = b.venue_id
+		INNER JOIN 
+		(SELECT COALESCE(MAX(score.total_scored) + 1, 0) AS runs_chased, venue_id
+		FROM (SELECT * FROM match WHERE venue_id = $1 ) AS venue_match
+		LEFT JOIN
+		(SELECT match_id, SUM(runs_scored + extra_runs) AS total_scored
+		FROM ball_by_ball
+		WHERE innings_no = 1
+		GROUP BY match_id, innings_no) AS score
+		ON venue_match.match_id = score.match_id
+		GROUP BY venue_id) AS c
+		ON b.venue_id = c.venue_id;`,
+        values: [venue_id],
+    }
+    pool.query(query, (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 //venues/:venue_id/pie_chart
 const getVenuePieChartInformation = function (request, response) {
     const venue_id = parseInt(request.params.venue_id)
     const query = {
-        text: `SELECT COALESCE(SUM(CASE WHEN win_type = 'runs' THEN 1 ELSE 0 END), 0) AS first_won,
+        text: `SELECT (first_won*1.0*100.0/total) AS first, (second_won*1.0*100.0/total) as second, (tie*1.0*100.0/total) as draw
+        FROM (SELECT COALESCE(SUM(CASE WHEN win_type = 'runs' THEN 1 ELSE 0 END), 0) AS first_won,
         COALESCE(SUM(CASE WHEN win_type = 'wickets' THEN 1 ELSE 0 END), 0) AS second_won,
-        COALESCE(SUM(CASE WHEN win_type NOT IN ('runs', 'wickets') THEN 1 ELSE 0 END), 0) AS draw,
+        COALESCE(SUM(CASE WHEN win_type NOT IN ('runs', 'wickets') THEN 1 ELSE 0 END), 0) AS tie,
         COALESCE (count(*), 0) AS total
         FROM match 
-        WHERE venue_id = $1;`,
+        WHERE venue_id = $1) AS abc;`,
         values: [venue_id],
     }
     pool.query(query,  (error, results) => {
