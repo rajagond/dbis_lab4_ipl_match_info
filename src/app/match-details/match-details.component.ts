2265e1fce65f5d18b513570d2692101028b56dd5
Raja Gond <rajagond@cse.iitb.ac.in>
2022-02-04 15:21:06 +0530

    all from my side

--- src/app/match-details/match-details.component.ts
+++ src/app/match-details/match-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
-import { MatchesDetailsService } from '../services/match-details.service';
+import { MatchDetailsService } from '../services/match-details.service';
 import { BatterInfo } from '../interfaces/match-info';
 
 @Component({
@@ -14,7 +14,9 @@ export class MatchDetailsComponent implements OnInit {
   displayedColumns2 = [ 'extras', 'totals'];
   displayedColumns3 = [ 'Bowler', 'balls_bowled', 'runs_given', 'wickets'];
   displayedColumns4 = [ 'Details']
-  displayedColumns5 = ['Playing_eleven_team1', 'Playing_eleven_team2']
+
+  displayedColumns5 = ['Playing_eleven_team1', 'Playing_eleven_team2'];
+
   dataSource1 = new MatTableDataSource<BatterInfo>([]);
   dataSource2 = new MatTableDataSource<BatterInfo>([]);
   dataSource3 = new MatTableDataSource<any>([]);
@@ -25,13 +27,22 @@ export class MatchDetailsComponent implements OnInit {
   dataSource8 = new MatTableDataSource<any>([]);
   dataSource9 = new MatTableDataSource<any>([]);
   link!: string | null;
+  Team: any ;
+  displayedColumns6!: string[];
 
-  constructor(private route:ActivatedRoute, private service: MatchesDetailsService ) { 
+  constructor(private route:ActivatedRoute, private service: MatchDetailsService ) { 
   }
 
   ngOnInit(): void {
     if(this.route.snapshot.paramMap.get('match_id'))
       this.link = this.route.snapshot.paramMap.get('match_id');
+
+    this.service.getInningTeamData(this.link + '/team').subscribe((data: any)=>{
+      console.log(data);
+      this.Team = data[0]
+      console.log(this.Team)
+    });
+
     this.service.getData(this.link + '/1').subscribe((data: any)=>{
       console.log(data.batsman);
       this.dataSource1.data = data.batsman;
@@ -47,8 +58,10 @@ export class MatchDetailsComponent implements OnInit {
     this.service.getData(this.link + '').subscribe((data: any)=>{
       this.dataSource7.data = data.basic_details;
       this.dataSource8.data = data.umpire;
+      const sr = "XI "
+      this.displayedColumns6 = [sr.concat(data.basic_details[0].team1.toString()), sr.concat(data.basic_details[0].team2.toString())]
       this.dataSource9.data = data.playing_eleven;
-      console.log(this.dataSource7.data);
+      console.log(this.dataSource9);
     });
   }
 
--- src/app/services/match-details.service.ts
+++ src/app/services/match-details.service.ts
@@ -7,15 +7,22 @@ import { catchError, Observable, retry, throwError } from 'rxjs';
 })
 
   
-export class MatchesDetailsService {
+export class MatchDetailsService {
 
   private API_SERVER = "http://localhost:3000/matches/match_info/";
+
+  private API_SERVER_TEAM = "http://localhost:3000/matches/";
 
   constructor(private http: HttpClient) { }
 
   public getData(link: string):Observable <any>{
     //let queryParams = new HttpParams().append("skip",skip).append("limit",limit);
     return this.http.get<any>(this.API_SERVER + link).pipe(retry(1), catchError(this.handleError));
+  }
+
+  public getInningTeamData(link: string):Observable <any>{
+    //let queryParams = new HttpParams().append("skip",skip).append("limit",limit);
+    return this.http.get<any>(this.API_SERVER_TEAM + link).pipe(retry(1), catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
